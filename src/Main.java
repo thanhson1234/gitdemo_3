732375404fb481bbec38cc578464f6933d3e6d02
khanhnguyen <khanhnguyen9329@gmail.com>
2022-12-07 11:02:22 +0700

    sout

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String name = "Khánh";
+        System.out.println(name);
+
+
+
     }
+
 }
