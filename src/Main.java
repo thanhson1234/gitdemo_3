aea73ef95e9d74c6062a2ac5d3830f8c4c0d7aa7
Viet0308 <phamviet0308@gmail.com>
2022-12-07 10:56:54 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String viet = "Viet";
+        System.out.println(viet);
+        System.out.println("Hello World!");
     }
 }
