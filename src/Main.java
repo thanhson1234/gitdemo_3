9545497f12f77e06abdce6eba0e7ee9403fd9e8a
huongtran2 <phuongtranth8888@gmail.com>
2022-12-07 10:56:37 +0700

    ..

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        String Huong = "Huong";
+        System.out.println(Huong);
         System.out.println("Hello world!");
     }
 }
