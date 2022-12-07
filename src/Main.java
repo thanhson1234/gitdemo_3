65a43318749508094e7c505e94ec32766a04c57b
Your Name <trangialochvtc@gmail.com>
2022-12-07 11:03:01 +0700

    loc

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        String loc = "loc";
+        System.out.println(loc);
         System.out.println("Hello world!");
     }
 }
