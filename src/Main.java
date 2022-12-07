949d0cc80dbb55f1b9128d40186fd2d607a5fc12
son <thanhson211095@gmail.com>
2022-12-07 10:57:04 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        String son = "son";
+        System.out.println(son);
         System.out.println("Hello world!");
     }
 }
